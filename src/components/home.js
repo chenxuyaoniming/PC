060158f7401c00843140d46cdc8d6780d008d6c5
GCF <aa843514778@qq.com>
2018-07-04 19:30:43 +0800

    gcf1

--- src/components/home.js
+++ src/components/home.js
@@ -8,26 +8,17 @@ class Home extends React.Component{
         super(props)
         this.state={
             banner:[],
-            city:[]
+            city:[],
+            city2:[]
         }
     }
 
     componentDidMount(){
         let _this = this;
         $.ajax({
-            type:'get',
-            url:'http://banner/api',
-            async:true,
-            dataType:'json',
-            success:function(data){
-                _this.setState({
-                    banner:data.banner
-                })
-            }
-        });
-        $.ajax({
-            type:'get',
+            type:'post',
             url:"http://bannerlist/api",
+            data:{id:1},
             async:true,
             dataType:'json',
             success:function(data){
@@ -36,40 +27,63 @@ class Home extends React.Component{
                 })
                 _this.state.city.map(function(item){
                     var tit = `<p>${item.tit}</p>`;
+                    var oI = `<i>1</i>`
                     var $oD = $('<div class="gBannerList"></div>')
                     var str1 = '';
                     item.city.map(function(a){
                         str1 += `<span>${a}</span>`
                     })
-                    $oD.append(tit,str1)
+                    $oD.append(tit,oI,str1)
                     $("#g-list-left").append($oD)
+                })
+            }
+        });
+        $.ajax({
+            type:'post',
+            url:"http://bannerlist/api",
+            data:{id:2},
+            async:true,
+            dataType:'json',
+            success:function(data){
+                _this.setState({
+                    city2:data.list
+                })
+                console.log(data.list)
+                _this.state.city2.map(function(item){
+                    var tit = item.city.tit
+                   var oD = $(`<div class="gListBox"><p>${tit}</p></div>`)
+                   var sec = item.city.second
+                   var str = '';
+                   console.log(sec)
+                   sec.map(function(item){
+                        str += `<span>${item}</span>`
+                   })
+                   oD.append(str)
+                   $("#g-list").append(oD)
                 })
             }
         })
     }
 
     
+    
 
     render(){
         return(
 
             <div id="gBanner-wrap">
-                {
-                    this.state.banner.map(function(item){
-                        return(
-                            <Link to="/"><img src={item.img} className="gBannerImg"/></Link>
-                        )
-                    })
-                }
-                <div id="g-list">
-                    <div id="g-list-left">
+            <Link to="/"><img src="http://www.aozhoutrip.com/uploads/2018/0316/f8dda50e1e258891487c843d04007e39_1920x420.jpg" className="gBannerImg"/></Link>
+            <Link to="/"><img src="http://www.aozhoutrip.com/uploads/2018/0415/d84066c742ca387ab49f0d6322c45076_1920x420.jpg" className="gBannerImg"/></Link>
 
-                        {/* <div id="g-list1" className=""></div> */}
+
+                <div id="g-list">
+                    <div id="g-list-left" ref='gListLeft'>
                     </div>
                 </div>
             </div>
         )
     }
+
 }
 
 
