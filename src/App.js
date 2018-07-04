8f3dbcdc4ac39eba271158b26728d38d4d60e92e
zhang heng <zhangheng@163.com>
2018-07-04 19:25:14 +0800

    zh1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {Link} from 'react-router';
+import './font/iconfont.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Zapp">
+          <div className="Zheader">
+            <div className="zheader_p"><span>您好！欢迎光临河南康辉国际旅行社有限责任公司</span></div>
+            <div  className="zheader_s"><span><Link to=''>登陆</Link></span><span><Link to=''>免费注册</Link></span><span><Link to=''><i className="icon iconfont icon-dingdan">订单查询</i></Link></span><span><Link to=''><i className="icon iconfont icon-fenqu">网站导航</i></Link></span></div>
+          </div>
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import {Router,Route,hashHistory,IndexRedirect,IndexRoute} from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
 import Home from './components/home'
 import './css/home.scss'
 import './css/index.scss'
+import './css/app.scss'
+import './font/iconfont.css'
 
 ReactDOM.render(<Router history={hashHistory}>
-    <Route path='/' component={App}>
-        <IndexRedirect to='/home' />
-    </Route>
+    <Route path='/' component={App}></Route>
     <Route path='/home' component={Home}></Route>
 </Router>, document.getElementById('root'));
 registerServiceWorker();
