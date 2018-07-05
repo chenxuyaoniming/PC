fc7bf749cf85d06887c763cbed19408848fd2aae
monster <457220728@qq.com>
2018-07-05 08:41:27 +0800

    lei 3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import {Router,Route,hashHistory,IndexRedirect,IndexRoute} from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
 import Home from './components/home'
+import About from './components/about'
 import './css/home.scss'
 import './css/index.scss'
+import './css/about.scss'
 
 ReactDOM.render(<Router history={hashHistory}>
-    <Route path='/' component={App}>
-        <IndexRedirect to='/home' />
-    </Route>
-    <Route path='/home' component={Home}></Route>
-</Router>, document.getElementById('root'));
+    <Route path='/' component={About}></Route>
+</Router>,document.getElementById('root'));
 registerServiceWorker();
