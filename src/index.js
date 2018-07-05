2ee2360e0e7e975c9d3b5aaa47a408c6ff672891
GCF <aa843514778@qq.com>
2018-07-05 21:16:12 +0800

    g主页

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './font/iconfont.css'
 import App from './App';
 import {Router,Route,hashHistory,IndexRedirect,IndexRoute} from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,8 +9,12 @@ import About from './components/about'
 import './css/home.scss'
 import './css/index.scss'
 import './css/about.scss'
+import './css/gg1.scss'
+import './css/aside.scss'
 
 ReactDOM.render(<Router history={hashHistory}>
-    <Route path='/' component={About}></Route>
+    <Route path='/' component={Home}>
+    </Route>
+    <Route path='/about/:id' component={About}></Route>
 </Router>,document.getElementById('root'));
 registerServiceWorker();
