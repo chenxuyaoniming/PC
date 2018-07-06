ef936be8b578ac92e70de197bbeeb4901459abf2
GCF <aa843514778@qq.com>
2018-07-06 21:43:31 +0800

    首页bug

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './font/iconfont.css'
 import App from './App';
-import {Router,Route,hashHistory,IndexRedirect,IndexRoute} from 'react-router';
+import {Router,Route,hashHistory,IndexRedirect,IndexRoute,Link} from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
 import Home from './components/home'
 import Retrieve from './components/retrieve'
 import Login from './components/login'
 import Register from './components/register'
 import About  from './components/about'
+import List from './components/list'
 import './css/home.scss'
 import './css/index.scss'
 import './css/about.scss'
@@ -18,11 +19,13 @@ import './css/login.scss'
 import './css/register.scss'
 import './css/retrieve.scss'
 import './css/app.scss'
+import './css/list.scss'
 
 ReactDOM.render(<Router history={hashHistory}>
     <Route path='/' component={App}>
         <IndexRoute component={Home} />
         <Route path='/home' component={Home}></Route>
+        <Route path='/list' component={List}></Route>
         <Route path='/login' component={Login}></Route>
         <Route path='/about/:id' component={About}></Route>
         <Route path='/register' component={Register}></Route>
