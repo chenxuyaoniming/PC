cdc4aa5ff72d3e9060147a81a2ddd4573468d297
mzk <mzk@163.com>
2018-07-08 17:50:21 +0800

    mzk

--- src/components/retrieve.js
+++ src/components/retrieve.js
@@ -13,12 +13,26 @@ class Retrieve extends React.Component{
             <div className='eve-big' style={homeimg}>
                 <div className='center'>
                     <p>找回密码</p>
-                {/* <Steps current={1} progressDot={customDot}>
-                    <Step title="Finished" description="You can hover on the dot." />
-                    <Step title="In Progress" description="You can hover on the dot." />
-                    <Step title="Waiting" description="You can hover on the dot." />
-                    <Step title="Waiting" description="You can hover on the dot." />
-                </Steps>, */}
+                    <div className='step'>
+                        <div className='step1'>
+                            <span className='step1-1'>
+                                <i>1</i>
+                                <em>验证身份</em>
+                            </span>
+                        </div>
+                        <div className='step2'>
+                        <span className='step2-1'>
+                                <i>2</i>
+                                <em>重置密码</em>
+                            </span>
+                        </div>
+                        <div className='step3'>
+                        <span className='step3-1'>
+                                <i>3</i>
+                                <em>完成</em>
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
